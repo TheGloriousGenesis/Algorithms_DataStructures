c305fab3c8de04769633fc55730685718ec8bfab
TheGloriousGenesis <am16g14@soton.ac.uk>
2020-04-11 23:29:47 +0100

    quick union improvements, refractor java syntax highlighter

--- src/main/QuickUnion.java
+++ src/main/QuickUnion.java
@@ -2,11 +2,16 @@ package main;
 
 public class QuickUnion {
     private int[] ids;
+    private int[] numberOfObjInTreeNode;
 
     public QuickUnion(final int n) {
         this.ids = new int[n];
+        this.numberOfObjInTreeNode = new int[n];
         for(int i=0; i<ids.length; i++) {
             ids[i] = i;
+        }
+        for(int i=0; i<numberOfObjInTreeNode.length; i++) {
+            numberOfObjInTreeNode[i] = 1;
         }
     }
 
@@ -23,25 +28,50 @@ public class QuickUnion {
         if (q >= ids.length || p >= ids.length) {
             return;
         }
-        int idQ = ids[q];
-        while(!(idQ == q)) {
-            idQ = ids[idQ];
+        int idQ = root(q);
+        int idP = root(p);
+        if(idQ == idP) {
+            return;
         }
-        ids[p] = idQ;
+        if(numberOfObjInTreeNode[idP] < numberOfObjInTreeNode[idQ]) {
+            ids[p] = idQ;
+            numberOfObjInTreeNode[idQ] += numberOfObjInTreeNode[idP];
+        } else {
+            ids[q] = idP;
+            numberOfObjInTreeNode[idP] += numberOfObjInTreeNode[idQ];
+        }
     }
 
     public boolean find(final int p, final int q) {
         if (q >= ids.length || p >= ids.length) {
             return false;
         }
-        int idQ = ids[q];
-        while(!(idQ == q)) {
-            idQ = ids[idQ];
-        }
-        int idP = ids[p];
-        while(!(idP == p)) {
-            idP = ids[idP];
-        }
+        int idQ = root(q);
+        int idP = root(p);
         return  idP == idQ;
+    }
+
+
+    private int root(final int element) {
+        int parent = ids[element];
+        while(!(parent == element)) {
+            // keeps tree flatter by assigning grandparent to parent at every other pass
+            ids[parent] = ids[ids[parent]];
+            parent = ids[parent];
+        }
+        return parent;
+    }
+
+    private int rootTwoPass(int element) {
+        int parent = ids[element];
+        while(!(parent == element)) {
+            parent = ids[parent];
+        }
+        while(!(ids[element] == parent)) {
+            int temp = ids[element];
+            ids[element] = parent;
+            element = temp;
+        }
+        return parent;
     }
 }
